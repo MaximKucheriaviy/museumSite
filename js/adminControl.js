6a7523ae0c639708816d9f8d95bc6c2ceb686f0f
Maxim Kucheriaviy <maxim-k@i.ua>
2022-03-25 12:46:13 +0200

    js to the tables

---
+++ js/adminControl.js
@@ -0,0 +1,12 @@
+document.querySelector('.Response-table-rq').onclick = function(event){
+    let target = event.target;
+    
+    if(target.classList.contains("cansel-button-red")){
+        let ID = target.parentElement.parentElement.querySelector('.ID-container').textContent
+        console.log("cansel button pressed " + ID);
+    }
+    else if(target.classList.contains("acsept-button-green")){
+        let ID = target.parentElement.parentElement.querySelector('.ID-container').textContent
+        console.log("submit button pressed " + ID);
+    }
+}
