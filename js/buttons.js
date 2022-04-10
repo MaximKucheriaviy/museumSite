e441b8b3288da75293cf2349139f395b3b76c479
Maxim Kucheriaviy <maxim-k@i.ua>
2022-04-10 20:48:11 +0300

    header done

--- js/buttons.js
+++ js/buttons.js
@@ -5,16 +5,33 @@ function showHide(currnet, target){
     target.classList.remove('is-hidden');
 }
 
+function openCloseControll(){
+    let iconS = document.querySelector('.header__controllbutton use:not(.is-hidden)');
+    let iconH = document.querySelector('.header__controllbutton .is-hidden');
+
+    iconS.classList.add('is-hidden');
+    iconH.classList.remove('is-hidden');
+    let menu = document.querySelector('.header__control');
+    if(menu.classList.contains('header--is-hidden')){
+        menu.classList.remove('header--is-hidden')
+    }
+    else{
+        menu.classList.add('header--is-hidden')
+    }
+}
+
 document.querySelector('.retistration-button').onclick = function(){
     let currnet = document.querySelector('.is-not-hidden');
     let target = document.querySelector('.registration');
     showHide(currnet, target);
+    openCloseControll()
 }
 
 document.querySelector('.main-button').onclick = function(){
     let currnet = document.querySelector('.is-not-hidden');
     let target = document.querySelector('.herro');
     showHide(currnet, target);
+    openCloseControll()
 }
 
 document.querySelector('.status-button').onclick = function(){
@@ -22,4 +39,9 @@ document.querySelector('.status-button').onclick = function(){
     let currnet = document.querySelector('.is-not-hidden');
     let target = document.querySelector('.status');
     showHide(currnet, target);
+    openCloseControll()
+}
+
+document.querySelector('.header__controllbutton').onclick = function(){
+    openCloseControll();
 }
